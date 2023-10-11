af76b7e14f8f7c3fda2afd107422f6ecf48f93b4
Niclas Schæffer <Niclasschaeffer96@gmail.com>
2023-10-11 16:04:37 +0200

    Basic ALU Works with Testing

--- codeA2/src/main/scala/ALU.scala
+++ codeA2/src/main/scala/ALU.scala
@@ -3,9 +3,37 @@ import chisel3.util._
 
 class ALU extends Module {
   val io = IO(new Bundle {
-    //Define the module interface here (inputs/outputs)
+    val f = Input(UInt(2.W))
+    val a = Input(UInt(32.W))
+    val b = Input(UInt(32.W))
+    val result = Output(UInt(32.W))
+    //probably not needed. we will never overflow this 32 bit ALU with a 20x20 pixel picture.
+    val overflow = Output(Bool())
+
   })
 
-  //Implement this module here
+  val result = Wire(UInt(32.W))
+  //default value
+  result := 0.U
+
+
+  //ALU functionallity Selection 
+  switch (io.f) {
+    is(0.U) {result := io.a + io.b}
+    is(1.U) {result := io.a - io.b}
+    is(2.U) {result := io.a | io.b}
+    is(3.U) {result := io.a & io.b}
+  }
+
+    //output
+  io.result := result
+
+   // Overflow detection
+  io.overflow := (io.f === 0.U && ((io.a(31) === 0.U && io.b(31) === 0.U && io.result(31) === 1.U) || 
+                 (io.a(31) === 1.U && io.b(31) === 1.U && io.result(31) === 0.U))) ||
+                 (io.f === 1.U && ((io.a(31) === 0.U && io.b(31) === 1.U && io.result(31) === 1.U) || 
+                 (io.a(31) === 1.U && io.b(31) === 0.U && io.result(31) === 0.U)))
+
+
 
 }
